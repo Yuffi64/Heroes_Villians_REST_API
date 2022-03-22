b31844dd6f894df7a1b97b3da40a9dcba94e48bc
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-22 01:19:23 -0500

    created serializer file.

---
+++ super_type/serializers.py
@@ -0,0 +1,8 @@
+
+from rest_framework import serializers
+from .models import Super_Type
+
+class SuperTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super_Type
+        fields = ['id','type'] 
