ed07ac401c27cf6491ec732827d8ebf94b9731b8
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-22 04:03:53 -0500

    added superuser admin

--- heroes_villains/settings.py
+++ heroes_villains/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'super_type',
+    'super_types',
     'supers',
     'rest_framework',
 ]
--- super_types/views.py
+++ super_types/views.py
@@ -1,12 +1,35 @@
 
 # Create your views here.
+from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperTypeSerializer 
+from .models import Super_Type
+
+
+
+@api_view(['GET', 'POST'])
+def super_types_list(request):
+   
+    if request.method == 'GET':
+        supers = Super_Type.objects.all()
+        serializer = SuperTypeSerializer(supers, many=True)
+        return Response(serializer.data)
+
+
+    elif request.method == 'POST':
+        serializer = SuperTypeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
 
 
 @api_view(['GET'])
-def super_types_list(response):
+def super_type_detail(request, pk):
+    super = get_object_or_404(Super_Type, pk=pk)
     
-    
-    
-    return Response('ok') 
+pass
+
