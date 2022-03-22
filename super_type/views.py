02a6512373a2e84e6637552e90f8803edd2db6c7
Yuffi64 <100430798+Yuffi64@users.noreply.github.com>
2022-03-22 02:01:56 -0500

    created urls file in super_type file + did a GET in postman.

--- super_type/views.py
+++ super_type/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+def super_type_list(response):
+    
+    
+    return Response('ok') 
